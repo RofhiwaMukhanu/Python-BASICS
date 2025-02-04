e8ac1d3831e25f2434e0a13dad0f50b780d414e1
DA M1 40 <dam140@DAs-MacBook-Air.local>
2025-02-04 12:00:03 +0200

    code updated

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 print('Hello Rofhiwa')# my name
 
-name=input('Whats for dinner')
+name=input('Whats your name')
 age=input('How old are you?')
+
+print('Hey' + name + 'Rofhiwa' )
+print('Your' + age + "18")
