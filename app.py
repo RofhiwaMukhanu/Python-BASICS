81a940657309f6a19b4a72c582ffff823fc628dc
DA M1 40 <dam140@DAs-MacBook-Air.local>
2025-02-07 12:36:40 +0200

    conditional statements

--- app.py
+++ app.py
@@ -10,15 +10,29 @@
 
 #if x > 2:
 
-print("you've gotten only 23")
+#print("you've gotten only 23")
 
-age = 11 
-has_id = True  
+#age = 11 
+#has_id = True  
 
-can_enter = age >= 18 and has_id
+#can_enter = age >= 18 and has_id
 
 
-if can_enter:
-    print("Access granted. Welcome to the rofh's club")
+#if can_enter:
+#print("Access granted. Welcome to the rofh's club")
+#else:
+    #print("Access denied. You are too young.")
+
+age =int(input("Enter your age: "))
+
+if age >= 18:
+    has_id = input("Do you have an ID? (yes/no): ")
+    
+    if has_id == "yes":
+        print("Access granted.")
+        
+    else:
+        print("Access denied. You need an ID.")
+        
 else:
-    print("Access denied. You are too young.")
+    print("Access denied. You must be at least 18 years old.")
