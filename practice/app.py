b8976f4182a6c58cd6462f273846844b13067104
DA M1 40 <dam140@DAs-MacBook-Air.local>
2025-02-07 09:37:39 +0200

    code updated

--- practice/app.py
+++ practice/app.py
@@ -1,10 +1,12 @@
 
 
-fruit= 'apple'
+fruit= 'apples'
 age = 35
 gpa = 10.22
+location= 'johannesburg'
  
 print('Hellow World')
 print("I am" ,age, "Years old" )
 print('I love' ,fruit, 'so much' )
 print('Your gpa is' ,gpa )
+print('I Stay in' ,location, )
