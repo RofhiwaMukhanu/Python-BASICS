15eb381a1885f28ec4c28fc55d0e0097ae01f453
DA M1 40 <dam140@DAs-MacBook-Air.local>
2025-02-07 13:48:29 +0200

    code updated

--- app.py
+++ app.py
@@ -23,16 +23,20 @@
 #else:
     #print("Access denied. You are too young.")
 
-age =int(input("Enter your age: "))
+#age =int(input("Enter your age: "))
 
-if age >= 18:
-    has_id = input("Do you have an ID? (yes/no): ")
+#if age >= 18:
+    #has_id = input("Do you have an ID? (yes/no): ")
     
-    if has_id == "yes":
-        print("Access granted.")
+    #if has_id == "yes":
+        #print("Access granted.")
         
-    else:
-        print("Access denied. You need an ID.")
+    #else:
+        #print("Access denied. You need an ID.")
         
-else:
-    print("Access denied. You must be at least 18 years old.")
+#else:
+    #print("Access denied. You must be at least 18 years old.")
+    
+fruits = ["apples", "bananas" ,"peaches","oranges","pears"]
+for fruit in fruits:
+       print(fruits)
