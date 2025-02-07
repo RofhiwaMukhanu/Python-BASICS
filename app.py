4f61ae7674238e22dbac6000a0ccfd71555a9e6c
DA M1 40 <dam140@DAs-MacBook-Air.local>
2025-02-07 11:48:02 +0200

    new code added

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-print('Hello Rofhiwa')# my name
+#print('Hello Rofhiwa')# my name
 
-name=input('Whats your name')
-age=input('How old are you?')
+#name=input('Whats your name')
+#age=input('How old are you?')
 
-print('Hey' + name + 'Rofhiwa' )
-print('Your' + age + "18")
+#print('Hey' + name + 'Rofhiwa' )
+#print('Your' + age + "18")
+
+x = 2
+
+if x > 2:
+   print("you've gotten only 23")
+
+elif x == 5:
+    print("goten just 2")
+    
+else:x
+print("such a low score")
